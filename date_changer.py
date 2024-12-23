f25060e30c820f6abb4d2f068d3449bbdfb81782
kreols <71571160+kreols@users.noreply.github.com>
2024-12-23 18:04:00 +0300

    Update date_changer.py
    123

--- date_changer.py
+++ date_changer.py
@@ -1,2 +1,3 @@
-issue_date = "25-12-2024"
-print(issue_date[0:5])
+created_date = input('Введите дату начала задачи в формате дд-мс-гд - ')
+issue_date = input('Введите дату конца задачи дд-мс-гд - ')
+print("Начало - " + created_date[0:5], "\nКонец - " + issue_date[0:5])
